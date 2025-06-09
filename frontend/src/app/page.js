7aac12e86f36f504544f74a6748f02454e5ed5e6
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-09 22:00:12 +0300

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -84,7 +84,7 @@ export default function Page() {
           {/* Номер телефона */}
           <div className="text-gray-200 font-medium hidden sm:block">
             Номер для связи:{" "}
-            <span className="text-white font-semibold">Жопа</span>
+            <span className="text-white font-semibold">попа</span>
           </div>
         </div>
       </header>
