6b1b274fa185afdb336249e20c0388d964e05ea4
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-09 23:41:32 +0300

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -31,7 +31,7 @@ export default function Page() {
       <header className="bg-black/60 text-white shadow fixed w-full z-20">
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
           {/* Левое меню */}
-          <nav className="flex space-x-6 items-center">
+          <nav className="flex space-x-3 items-center">
             {/* Каталог с выпадающим меню */}
             <div className="relative group">
               <button className="px-6 py-2 bg-orange-600/70 hover:bg-orange-600/90 text-white rounded-full shadow transition-colors text-lg">
