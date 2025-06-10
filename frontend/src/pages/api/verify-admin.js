76ddc67ec4005b39a7b4b5bef2bdf89136bc3e81
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-10 20:58:26 +0300

    Create verify-admin.js

---
+++ frontend/src/pages/api/verify-admin.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const { admin } = req.query;
+  const adminSecret = process.env.ADMIN_SECRET;
+
+  if (admin === adminSecret) {
+    res.status(200).json({ access: true });
+  } else {
+    res.status(401).json({ access: false });
+  }
+}
