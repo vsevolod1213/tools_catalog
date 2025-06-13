85b02a05967c3629be934d4491dc719157a2c563
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-13 18:45:42 +0300

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -101,7 +101,7 @@ export default function Page() {
 
           <div
             className={`transition-all duration-300 ease-in-out z-10 ${
-              !isSticky
+              isSticky
                 ? "fixed top-[72px] left-1/2 -translate-x-1/2 w-full max-w-md px-6"
                 : "relative mt-8 mx-auto w-full px-4"
             }`}
