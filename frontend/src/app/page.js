5752b93bc278be908b4381444115d257ea582142
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-13 18:06:35 +0300

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -154,7 +154,7 @@ export default function Page() {
                             )}
                           </div>
                           <h3 className="text-lg font-semibold">{product.name}</h3>
-                          <p className="text-sm text-gray-700 min-h-[48px]">{product.description}</p>
+                          <p className="text-sm text-gray-700 opacity-0 max-h-0 group-hover:opacity-100 group-hover:max-h-32 transition-all duration-300 overflow-hidden">{product.description}</p>
                         </div>
                         <div className="mt-2 flex justify-between items-center">
                           <div className="font-bold text-xl text-green-700">{product.price} ₽</div>
