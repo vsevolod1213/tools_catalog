3a0bbfe0f257f8981dcb71ef3222f15ad419c389
vsevolod1213 <lvsevolod201@gmail.com>
2025-06-17 21:17:17 +0300

    d

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
   title: "Каталог",
   description: "Описание сайта",
   icons: {
-    icon: "/favicon-v2.ico", // <-- добавь сюда путь к иконке
+    icon: "/analiz.ico", // <-- добавь сюда путь к иконке
   },
 };
 
